00b77d3d4b00b8dffd37acb06ec35eea69c48c1d
Vaibhav Mane <vaibhavmane101@gmail.com>
2017-07-16 15:55:56 +0530

    my shopping project setup complete...

---
+++ gamingbackend/src/main/java/onlineshoping/gamingbackend/App.java
@@ -0,0 +1,13 @@
+package onlineshoping.gamingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
